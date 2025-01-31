1d484b2e425c6cc383cae4e26ed0ff13a6ede960
Yujan Bajagain <98070107+GamingClaus@users.noreply.github.com>
2025-01-31 16:19:41 +0545

    Added Replayablity

--- src/GameSystem.java
+++ src/GameSystem.java
@@ -9,78 +9,67 @@ public class GameSystem {
     String[] maxChoices = new String[]{"rock","paper","scissor"};
     int aiScore = 0;
     int playerScore = 0;
+    String replay;
 
     GameSystem() {
 
         String playerHand = "";
 
         boolean gameon = true;
-        while (gameon) {
-            String aiChoice = AiSystem(); // getting the Computer's Choice.
-            while (!Arrays.asList(maxChoices).contains(playerHand)) {
-                System.out.print("Choose Rock,Paper or Scissor:");
-                playerHand = scanner.next(); //only takes the first word by next.
+        do{
+            gameon = true;
+            aiScore = 0;
+            playerScore = 0;
+            while (gameon) {
+
+                playerHand = "";
+                String aiChoice = AiSystem(); // getting the Computer's Choice.
+                while (!Arrays.asList(maxChoices).contains(playerHand)) {
+                    System.out.print("Choose Rock,Paper or Scissor:");
+                    playerHand = scanner.next().toLowerCase(); //only takes the first word by next.
+
+                    if (!Arrays.asList(maxChoices).contains(playerHand)) {
+                        System.out.println("Choose a proper Option.");
+                    }
+                }
 
 
+                determineWinner(playerHand,aiChoice);
+                if(aiScore==3 || playerScore==3){
+                    gameon =false;
             }
-            determineWinner(playerHand,aiChoice);
-            playerHand = playerHand.toLowerCase();
-            System.out.print("Choose Rock,Paper or Scissor:");
-            playerHand = scanner.next();
-
-            if(aiScore==2 || playerScore==2){
-                gameon =false;
-            }
-
         }
-
-        if (aiScore>playerScore){
-            System.out.println("Computer Won with " + (aiScore+1) +" points");
+            System.out.println("Your Score: " + playerScore +" Computer Score: " + aiScore);
+            if (aiScore>playerScore){
+            System.out.println("Computer Won with " + (aiScore) +" points");
         }
         else{
-            System.out.println("You Won with " + (playerScore+1) +" points");
+            System.out.println("You Won with " + (playerScore) +" points");
         }
+        System.out.println("Do you want to play again? (Press Y or Yes)");
+        replay = scanner.next().toLowerCase();
+    }while(replay.equals("y") || replay.equals("yes"));
     }
 
 
     private String AiSystem(){
         int aiRNG = splittableRandom.nextInt(3); // generates random numbers from 0-2.
-        switch (aiRNG){
-            case 0:
-                return "paper";
-            case 1:
-                return "rock";
-            case 2:
-                return "scissor";
-        }
-        return "";
+        return maxChoices[aiRNG];
     }
 
     private void determineWinner(String playerHand,String aiChoice){
+        System.out.println("You chose " + playerHand + " and the computer chose " + aiChoice + ".");
 
-        if (playerHand.equals(aiChoice)){
-            System.out.println("You chose " + playerHand + "and Computer chose " + aiChoice);
-            System.out.println("Its a Tie.");
-        }
-        else if (playerHand.equals("rock") && aiChoice.equals("scissor")) {
-            System.out.println("You chose "+ playerHand +" and Computer chose " + aiChoice);
+        if (playerHand.equals(aiChoice)) {
+            System.out.println("It's a Tie.");
+        } else if ((playerHand.equals("rock") && aiChoice.equals("scissor")) ||
+                (playerHand.equals("paper") && aiChoice.equals("rock")) ||
+                (playerHand.equals("scissor") && aiChoice.equals("paper"))) {
             System.out.println("You got a Score.");
-            playerScore++; // because player won.
-        }
-        else if (playerHand.equals("paper") && aiChoice.equals("rock")) {
-            System.out.println("You chose "+ playerHand +" and Computer chose " + aiChoice);
-            System.out.println("You got a Score.");
-            playerScore++; // because player won.
-        }
-        else if (playerHand.equals("scissor") && aiChoice.equals("paper")) {
-            System.out.println("You chose "+ playerHand +" and Computer chose " + aiChoice);
-            System.out.println("You got a Score.");
-            playerScore++; // because player won.
-        }
-        else{
-            System.out.println("You chose "+ playerHand +" and Computer chose " + aiChoice);
+            playerScore++;
+        } else {
             System.out.println("Computer got a Score.");
-            aiScore++; // because computer won.
+            aiScore++;
         }
     }
 
